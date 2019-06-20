e49af210a43d6216a9ded72657d15a6b2cd40dd1
KMDMNAK <30950010+KMDMNAK@users.noreply.github.com>
2019-06-20 20:23:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.2",
     install_requires=["gensim","matplotlib","networkx","mecab-python3","googletrans"],
     package_dir={'home':'','parse':"parse","network":"network","gensim":"GensimWrapper"},
-    packages=find_packages()
+    #packages=find_packages()
+    packages=["home","parse","network","gensim"]
 )
 
