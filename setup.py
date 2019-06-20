6363bf5af653a98c810924c70f4f7b045a60305a
KMDMNAK <kmdmuneaki@gmail.com>
2019-06-20 20:30:46 +0900

    modified

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="psWord",
     version="1.0.2",
     install_requires=["gensim","matplotlib","networkx","mecab-python3","googletrans"],
-    package_dir={'home':'','parse':"parse","network":"network","gensim":"GensimWrapper"},
+    package_dir={'home':'','parse':"psWord/parse","network":"psWord/network","gensim":"psWord/GensimWrapper"},
     packages=find_packages()
     #packages=["home","parse","network","gensim"]
 )
