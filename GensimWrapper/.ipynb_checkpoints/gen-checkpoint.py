6f8286f8a635ecb30854d347d8e5e58e9e9a520a
KMDMNAK <kmdmuneaki@gmail.com>
2019-06-20 20:43:37 +0900

    delete some files

--- GensimWrapper/.ipynb_checkpoints/gen-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import gensim
-
-def get_model():
-    return gensim.models.KeyedVectors.load_word2vec_format("model.vec")
--- GensimWrapper/gen.py
+++
@@ -1,7 +0,0 @@
-import gensim
-
-def getModel(dictionary_pass):
-    """
-        get word's vector from the dictionary
-    """
-    return gensim.models.KeyedVectors.load_word2vec_format(dictionary_pass)
