f0d3a1ae7baf718a065690efdbc6dc102c6d736e
KMDMNAK <30950010+KMDMNAK@users.noreply.github.com>
2019-06-25 00:40:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="psWord",
-    version="1.0.2",
+    version="1.0.3",
     install_requires=["gensim","matplotlib","networkx","mecab-python3","googletrans"],
     package_dir={'home':'','parse':"psWord/parse","network":"psWord/network","gensim":"psWord/GensimWrapper"},
     packages=find_packages()
