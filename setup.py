d9811ce9b391fffa3ab345a3fd3f86e9969b4a95
KMDMNAK <30950010+KMDMNAK@users.noreply.github.com>
2019-07-05 15:25:56 +0900

    delete some required packages for TEST

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 setup(
     name="psWord",
     version="1.0.3",
-    install_requires=["gensim","matplotlib","networkx","mecab-python3","googletrans"],
+    install_requires=["googletrans","mecab-python3"],#["gensim","matplotlib","networkx","mecab-python3","googletrans"],
     package_dir={'home':'','parse':"psWord/parse","network":"psWord/network","gensim":"psWord/GensimWrapper"},
     packages=find_packages()
     #packages=["home","parse","network","gensim"]
