075e1da41eb2783e45b2517135966647ad76cd8e
KMDMNAK <kmdmuneaki@gmail.com>
2019-10-16 21:26:50 +0900

    delete jupyter note

--- psWord/parse/.ipynb_checkpoints/translate-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import googletrans
-
-def get_transtext(text,dest='ja'):
-    """
-        dest which is the language to read in ["ja","en"]
-    """
-    translator=googletrans.Translator()
-    return translator.translate(text,dest=dest).text
